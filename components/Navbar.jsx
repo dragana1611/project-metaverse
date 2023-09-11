4cc589002bbcb1523dd9f120f34cac2e6b32eb77
Dragana Petrovic <dragana.popov-petrovic@badin.rs>
2023-09-11 21:04:48 +0200

    initial commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+import styles from "../styles";
+
+const Navbar = () => <nav>navbar</nav>;
+
+export default Navbar;
+
+/* // 'use client' is needed only if we’re using React Server Components or building a library compatible with them.* */
